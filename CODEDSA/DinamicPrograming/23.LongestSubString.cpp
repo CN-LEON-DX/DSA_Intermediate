8b64cbeee81cfeb792a45a28c90e977fdd217088
FuzzilFox144 <125853450+CN-LEON-DX@users.noreply.github.com>
2023-12-22 23:58:41 +0700

    22/12/2023
    Bad

--- CODEDSA/DinamicPrograming/23.LongestSubString.cpp
+++ CODEDSA/DinamicPrograming/23.LongestSubString.cpp
@@ -1,33 +1,3 @@
-// Một tên trộm có 1 cái túi có thể mang các đố vật với trọng lượng tối đa là V. Hiện tại tên trộm muốn lựa chọn các đồ vật trong N đồ vật để ăn trộm, mỗi đồ vật có trọng lượng là w[i] và giá trị là v[i]. Hãy xác định tổng giá trị lớn nhất của các đồ vật mà tên trộm này lựa chọn sao cho trọng lượng của chúng không vượt quá V.
-
-// Input Format
-
-// Dòng đầu ghi 2 số N và V. Dòng tiếp theo ghi N số của mảng w. Sau đó là một dòng ghi N số của mảng v.
-
-// Constraints
-
-// V<=1000; N≤1000; 1<=w[i], c[i]<=500;
-
-// Output Format
-
-// In ra giá trị lớn nhất có thể đạt được.
-
-// Sample Input 0
-
-// 6 22
-// 39 44 4 59 91 70 
-// 47 26 92 33 6 69 
-// Sample Output 0
-
-// 92
-// Sample Input 1
-
-// 7 22
-// 10 3 22 50 83 16 41 
-// 99 97 54 19 50 70 48 
-// Sample Output 1
-
-// 196
 #include <bits/stdc++.h>
 using namespace std;
 // Run Time 
@@ -63,22 +33,11 @@ const int dx[4] = {-1, +0, +1, +0};
 const int dy[4] = {+0, +1, +0, -1};
 const int moveX[8] = {+0, +0, +1, -1, -1, -1, +1, +1};
 const int moveY[8] = {+1, -1, +0, +0, -1, +1, +1, -1};
+
 const int MOD = 1e9+7;
 typedef long long ll;
-int n, s;
-void find(vector<int> w, vector<int> v){
-    vector<vector<int>> dp(n+1, vector<int>(s+1, 0));
-    for (int i = 1;i<=n;i++){
-        for (int j = 0;j<=s;j++){
-            if (j < w[i])
-                dp[i][j] = dp[i-1][j];
-            else
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-w[i]] + v[i]);
-        }
-    }
-    debug(dp);
-    cout << dp[n][s];
-}
+int n;
+
 int main(){
 	#ifndef ONLINE_JUDGE
 	// for getting input from input.txt
@@ -86,12 +45,26 @@ int main(){
 	// for writing output to output.txt
 	freopen("out.txt", "w", stdout);
 	#endif
-	cin >> n >> s;
-	vector<int> w(n+1);
-	vector<int> v(n+1);
-	for (int i = 1;i<=n;i++) cin >> w[i];
-	for (int j = 1;j<=n;j++) cin >> v[j];
-	debug(w); debug(v);
-	find(w, v);
+	string s; cin >> s;
+	int t = s.size();
+	vector<vector<int>> dp(t + 1, vector<int>(t + 1, 0));
+	// Khoi tao ma tran cho dp truoc i = j => dp = 1;
+	for (int i = 0;i<=t;i++){
+		dp[i][i] = 1;
+	}
+	int res = 1;
+	for (int i = 0; i< t; i++){
+		for (int j = 1; j <= t;j++){
+			if (j > i + 1){
+				cout << s[i] << " " << s[j-1] << endl;
+				if (dp[i+2][j-1] && s[i] == s[j-1]) dp[i+1][j] = 1;
+				if (dp[i+1][j]){
+					res = max(j - i + 1, res);
+				}
+			}
+		}
+	}
+	debug(dp);
+	cout << res;
 	return 0;
 }
--- CODEDSA/DinamicPrograming/4.SubSetHasSumK.cpp
+++ CODEDSA/DinamicPrograming/4.SubSetHasSumK.cpp
@@ -10,33 +10,64 @@
 // 69 16 82 170 31 24 45 112 
 // Sample Output 0
 // 1
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-bool subsetSum(vector<int>& A, int S) {
-    int N = A.size();
-    vector<vector<bool>> dp(N + 1, vector<bool>(S + 1, false));
-    for (int i = 0; i <= N; i++) {
-        dp[i][0] = true;
-    }
-    for (int i = 1; i <= N; i++) {
-        for (int j = 1; j <= S; j++) {
-            if (A[i - 1] <= j) {
-                dp[i][j] = dp[i - 1][j - A[i - 1]] || dp[i - 1][j];
-            } else {
-                dp[i][j] = dp[i - 1][j];
+// Run Time 
+#define TIME cout << "\nTime: " << (1.0 * clock() / CLOCKS_PER_SEC) << "s"
+// Debug
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+typedef pair<int,int> ii;
+const int dx[4] = {-1, +0, +1, +0};
+const int dy[4] = {+0, +1, +0, -1};
+const int moveX[8] = {+0, +0, +1, -1, -1, -1, +1, +1};
+const int moveY[8] = {+1, -1, +0, +0, -1, +1, +1, -1};
+const int MOD = 1e9+7;
+typedef long long ll;
+int n, s;
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    // for getting input from input.txt
+    freopen("in.txt", "r", stdin);
+    // for writing output to output.txt
+    freopen("out.txt", "w", stdout);
+    #endif
+    cin >> n >> s;
+    int a[n+1];
+    for (int i = 1;i<=n;i++) cin >> a[i];
+    vector<int> dp(s+1, 0);
+    dp[0] = 1;
+    for (int i = 1;i<=n;i++){
+        for (int j = s;j>= 1;--j){
+            if (j >= a[i]){
+                if (dp[j - a[i]]) dp[j] = 1;
             }
         }
     }
-    return dp[N][S];
-}
-int main() {
-    int N, S;
-    cin >> N >> S;
-    vector<int> A(N);
-    for (int i = 0; i < N; i++) {
-        cin >> A[i];
-    }
-    cout << (subsetSum(A, S) ? 1 : 0) << endl;
+    cout << dp[s];
     return 0;
 }
